c8da9e597ccbacb58574abb826869442607f3853
Arnaud Brousseau <rno@turnkey.io>
2024-02-22 11:55:26 -0600

    Fix Rubocop-flagged offenses

--- examples/whoami/whoami.rb
+++ examples/whoami/whoami.rb
@@ -6,19 +6,18 @@ require "json"
 Dotenv.load
 
 if ENV["TURNKEY_ORGANIZATION_ID"].nil?
-    raise "Please set TURNKEY_ORGANIZATION_ID in your .env file"
+  raise "Please set TURNKEY_ORGANIZATION_ID in your .env file"
 end
 
 # Make a whoami request
 begin
-    client = TurnkeyClient.configure do |c|
-        c.api_public_key = ENV["TURNKEY_API_PUBLIC_KEY"]
-        c.api_private_key = ENV["TURNKEY_API_PRIVATE_KEY"]
-    end
+  client = TurnkeyClient.configure do |c|
+    c.api_public_key = ENV["TURNKEY_API_PUBLIC_KEY"]
+    c.api_private_key = ENV["TURNKEY_API_PRIVATE_KEY"]
+  end
 
-    response = TurnkeyClient::SessionsApi.new(client).get_whoami({ organization_id: ENV["TURNKEY_ORGANIZATION_ID"]})
-    puts response.to_hash
+  response = TurnkeyClient::SessionsApi.new(client).get_whoami({ organization_id: ENV["TURNKEY_ORGANIZATION_ID"] })
+  puts response.to_hash
 rescue TurnkeyClient::ApiError => e
-    puts "Exception when calling Whoami endpoint: #{e}"
+  puts "Exception when calling Whoami endpoint: #{e}"
 end
-
